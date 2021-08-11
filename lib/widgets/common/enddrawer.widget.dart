355b79a22c05ac9f2c290e87223000a59b46c9a5
João Rafa <joaopedrorafael19@gmail.com>
2021-08-10 21:13:42 -0300

    end drawer

---
+++ lib/widgets/common/enddrawer.widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EndDrawer extends StatelessWidget {
+  const EndDrawer({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      
+    );
+  }
+}
