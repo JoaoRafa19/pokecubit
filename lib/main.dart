83bf757f9bbbc8b7787c378045edc11479fea0cb
João Rafa <joaopedrorafael19@gmail.com>
2021-08-12 10:37:28 -0300

    checando se o usuário está logado ou se é um usuário anonimo

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:poke_cubit/modules/home/cubit/home_cubit.dart';
+import 'package:poke_cubit/modules/home/home.page.dart';
 import 'package:poke_cubit/modules/login/cubit/login_cubit.dart';
 import 'package:poke_cubit/modules/login/login.page.dart';
 
@@ -11,21 +13,31 @@ Future<void> main() async {
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   static final FirebaseAuth auth = FirebaseAuth.instance;
-
+  MyApp() {
+    if (auth.currentUser != null) {
+      if (auth.currentUser!.isAnonymous) {
+        auth.signOut();
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PokeCubit',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, textTheme: GoogleFonts.robotoTextTheme()),
-      home: auth.currentUser == null
+      home: auth.currentUser == null || auth.currentUser!.isAnonymous
           ? BlocProvider<LoginCubit>(
               create: (context) => LoginCubit(),
               child: LoginPage(),
             )
-          : Container(),
+          : BlocProvider<HomeCubit>(
+              create: (context) => HomeCubit(),
+              child: HomePage(),
+            ),
     );
   }
 }
