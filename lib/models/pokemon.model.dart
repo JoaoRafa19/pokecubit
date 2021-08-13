7c05d6df369245fb35b3d402d08bec6ca140a043
João Rafa <joaopedrorafael19@gmail.com>
2021-08-13 11:29:16 -0300

    pokemon model

---
+++ lib/models/pokemon.model.dart
@@ -0,0 +1,210 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class PokeAPI {
+  List<Pokemon>? pokemons = [];
+
+  PokeAPI({this.pokemons});
+
+  PokeAPI.fromJson(Map<String, dynamic> json) {
+    if (json['pokemon'] != null) {
+      try {
+        pokemons = <Pokemon>[];
+        json['pokemon'].forEach((pokemon) {
+          pokemons?.add(new Pokemon.fromJson(pokemon));
+        });
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.pokemons != null) {
+      data['pokemon'] = this.pokemons?.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Pokemon {
+  int? id;
+  String? num;
+  String? name;
+  String? img;
+  List<String>? type;
+  String? height;
+  String? weight;
+  String? candy;
+  int? candyCount;
+  late Color bgcolor;
+  String? egg;
+  String? spawnTime;
+  List<String>? weaknesses;
+  List<NextEvolution>? nextEvolution;
+  List<PrevEvolution>? prevEvolution;
+
+  Pokemon({this.id, this.num, this.name, this.img, this.type, this.height, this.weight, this.candy, this.candyCount, this.egg, this.spawnTime, this.weaknesses, this.nextEvolution, this.prevEvolution});
+
+  Pokemon.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    num = json['num'];
+    name = json['name'];
+    img = json['img'].replaceFirst("http", "https");
+    type = json['type'].cast<String>();
+    height = json['height'];
+    weight = json['weight'];
+    candy = json['candy'];
+    candyCount = json['candy_count'];
+    egg = json['egg'];
+    spawnTime = json['spawn_time'];
+    weaknesses = json['weaknesses'].cast<String>();
+    if (json['next_evolution'] != null) {
+      nextEvolution = <NextEvolution>[];
+      json['next_evolution'].forEach((evolution) {
+        nextEvolution?.add(new NextEvolution.fromJson(evolution));
+      });
+    }
+    if (json['prev_evolution'] != null) {
+      prevEvolution = <PrevEvolution>[];
+      json['prev_evolution'].forEach((evolution) {
+        prevEvolution?.add(new PrevEvolution.fromJson(evolution));
+      });
+    }
+  }
+  get color {
+    switch (this.type![0]) {
+      case 'Normal':
+        return Colors.brown[400];
+        break;
+      case 'Fire':
+        return Colors.red;
+        break;
+      case 'Water':
+        return Colors.blue;
+        break;
+      case 'Grass':
+        return Colors.green;
+        break;
+      case 'Electric':
+        return Colors.amber;
+        break;
+      case 'Ice':
+        return Colors.cyanAccent[400];
+        break;
+      case 'Fingting':
+        return Colors.orange;
+        break;
+      case 'Poison':
+        return Colors.purple;
+        break;
+      case 'Ground':
+        return Colors.orange[300];
+        break;
+      case 'Flying':
+        return Colors.indigo[200];
+        break;
+      case 'Psychic':
+        return Colors.pink;
+        break;
+      case 'Bug':
+        return Colors.lightGreen[500];
+        break;
+      case 'Rock':
+        return Colors.grey;
+        break;
+      case 'Ghost':
+        return Colors.indigo[400];
+        break;
+      case 'Dark':
+        return Colors.brown;
+        break;
+      case 'Dragon':
+        return Colors.indigo[800];
+        break;
+      case 'Steel':
+        return Colors.blueGrey;
+        break;
+      case 'Fairy':
+        return Colors.pinkAccent;
+        break;
+      default:
+        return Colors.grey;
+        break;
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['num'] = this.num;
+    data['name'] = this.name;
+    data['img'] = this.img;
+    data['type'] = this.type;
+    data['height'] = this.height;
+    data['weight'] = this.weight;
+    data['candy'] = this.candy;
+    data['candy_count'] = this.candyCount;
+    data['egg'] = this.egg;
+    data['spawn_time'] = this.spawnTime;
+    data['weaknesses'] = this.weaknesses;
+    if (this.nextEvolution != null) {
+      data['next_evolution'] = this.nextEvolution!.map((v) => v.toJson()).toList();
+    }
+    if (this.prevEvolution != null) {
+      data['prev_evolution'] = this.prevEvolution!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+
+  Widget getImage({required double width,required double heigh}) {
+    return CachedNetworkImage(
+      height: heigh,
+      width: width,
+      imageUrl: "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/$num.png",
+      placeholder: (BuildContext context, url) => SpinKitCircle(
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+class NextEvolution {
+  String? num;
+  String? name;
+
+  NextEvolution({this.num, this.name});
+
+  NextEvolution.fromJson(Map<String, dynamic> json) {
+    num = json['num'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['num'] = this.num;
+    data['name'] = this.name;
+    return data;
+  }
+}
+
+class PrevEvolution {
+  late String num;
+  late String name;
+
+  PrevEvolution({required this.num, required this.name});
+
+  PrevEvolution.fromJson(Map<String, dynamic> json) {
+    num = json['num'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['num'] = this.num;
+    data['name'] = this.name;
+    return data;
+  }
+}
--- lib/modules/home/home.page.dart
+++ lib/modules/home/home.page.dart
@@ -6,6 +6,56 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), endDrawer: EndDrawer());
+    var mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      appBar: AppBar(),
+      endDrawer: EndDrawer(),
+      body: SafeArea(
+        child: Center(
+          child: Stack(
+            //fit: StackFit.passthrough,
+            alignment: Alignment.topCenter,
+            clipBehavior: Clip.none,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 20, left: mediaQuery.size.width * 0.08),
+                child: Column(
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            child: Text(
+                              "Pokedex",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 29,
+                                fontFamily: 'Google',
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+                    width: mediaQuery.size.width * 0.9,
+                    color: Colors.white30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    
+    );
   }
 }
