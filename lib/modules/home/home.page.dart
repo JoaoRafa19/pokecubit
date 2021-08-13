ffa393b3d088831ba261be93caa841d1a16ffcff
João Pedro <joaopedrorafael19@gmail.com>
2021-08-13 18:04:01 -0300

    open scaffold drawer

--- lib/modules/home/home.page.dart
+++ lib/modules/home/home.page.dart
@@ -9,83 +9,109 @@ import 'package:poke_cubit/widgets/home/pokemon.card.dart';
 import 'cubit/home_cubit.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> _scaffoldkey = new GlobalKey();
+  HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    var mediaQuery = MediaQuery.of(context);
+  Widget build(BuildContext buildContext) {
+    var mediaQuery = MediaQuery.of(buildContext);
     return Scaffold(
-      appBar: AppBar(),
+      key: _scaffoldkey,
       endDrawer: EndDrawer(),
       body: SafeArea(
-        child: Center(
-          child: Stack(
-            //fit: StackFit.passthrough,
-            alignment: Alignment.topCenter,
-            clipBehavior: Clip.none,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 20, left: mediaQuery.size.width * 0.08),
-                child: Column(
-                  children: [
-                    Container(
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(top: mediaQuery.size.height * 0.01),
-                            child: Text(
-                              "Pokedex",
-                              style: GoogleFonts.lato(fontSize: 30, fontWeight: FontWeight.w800),
-                            ),
-                          ),
-                        ],
-                      ),
+        child: Padding(
+          padding: EdgeInsets.only(top: mediaQuery.size.height * 0.1),
+          child: Center(
+            child: Stack(
+              //fit: StackFit.passthrough,
+              alignment: Alignment.topCenter,
+              clipBehavior: Clip.none,
+              children: <Widget>[
+                Positioned(
+                  right: -150,
+                  top: -190,
+                  child: Stack(children: [
+                    Image.asset(
+                      'assets/pokeball_white.png',
+                      color: Colors.grey[300],
+                      width: mediaQuery.size.width * 0.9,
                     ),
-                  ],
+                    Positioned(
+                        top: 165,
+                        right: 160,
+                        child: GestureDetector(
+                          child: Icon(Icons.menu, size: 50),
+                          onTap: () {
+                            if (_scaffoldkey.currentState != null) {
+                              _scaffoldkey.currentState!.openEndDrawer();
+                            }
+                          },
+                        ))
+                  ]),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white30,
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-                    ),
-                    width: mediaQuery.size.width * 0.9,
-                    height: mediaQuery.size.height * 0.8,
-                    child: BlocConsumer<HomeCubit, HomeState>(
-                        listener: (context, state) {},
-                        builder: (context, state) {
-                          if (state is HomeLoaded) {
-                            return GridView.builder(
-                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                                itemCount: state.pokeapi.pokemons!.length,
-                                itemBuilder: (item, index) {
-                                  return AnimationConfiguration.staggeredGrid(
-                                    position: index,
-                                    columnCount: 3,
-                                    child: ScaleAnimation(
-                                      child: GestureDetector(
-                                        child: Padding(
-                                          padding: EdgeInsets.all(8),
-                                          child: PokeCard(
-                                            index: index,
-                                            pokemon: state.pokeapi.pokemons![index],
+                Container(
+                  padding: EdgeInsets.only(top: 20, left: mediaQuery.size.width * 0.08),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: mediaQuery.size.width / 100),
+                              child: Text(
+                                "Pokedex",
+                                style: GoogleFonts.nunito(fontSize: 50, fontWeight: FontWeight.w800, color: Colors.grey[700]),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white30,
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(20)),
+                      ),
+                      width: mediaQuery.size.width * 0.9,
+                      height: mediaQuery.size.height * 0.8,
+                      child: BlocConsumer<HomeCubit, HomeState>(
+                          listener: (context, state) {},
+                          builder: (context, state) {
+                            if (state is HomeLoaded) {
+                              return GridView.builder(
+                                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                                  itemCount: state.pokeapi.pokemons!.length,
+                                  itemBuilder: (item, index) {
+                                    return AnimationConfiguration.staggeredGrid(
+                                      position: index,
+                                      columnCount: 3,
+                                      child: ScaleAnimation(
+                                        child: GestureDetector(
+                                          child: Padding(
+                                            padding: EdgeInsets.all(8),
+                                            child: PokeCard(
+                                              index: index,
+                                              pokemon: state.pokeapi.pokemons![index],
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                  );
-                                });
-                          } else {
-                            return SpinKitCubeGrid(color: Colors.black);
-                          }
-                        }),
+                                    );
+                                  });
+                            } else {
+                              return SpinKitCubeGrid(color: Colors.black);
+                            }
+                          }),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
