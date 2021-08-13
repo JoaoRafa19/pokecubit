bc43ba7fbf15937e7179046b92a9e55ff1f6669a
João Rafa <joaopedrorafael19@gmail.com>
2021-08-13 10:29:02 -0300

    login page

--- lib/modules/login/login.page.dart
+++ lib/modules/login/login.page.dart
@@ -12,64 +12,62 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
     return Scaffold(
+      backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Container(
-        color: Colors.white,
-        child: Stack(
-          children: [
-            Center(
-              heightFactor: mediaQuery.size.height * 0.001,
-              child: Container(
-                width: mediaQuery.size.height * .3,
-                child: Image(
-                  fit: BoxFit.fill,
-                  image: pokeballImage,
-                ),
+      body: Stack(
+        children: [
+          Center(
+            heightFactor: mediaQuery.size.height * 0.001,
+            child: Container(
+              width: mediaQuery.size.height * .3,
+              child: Image(
+                fit: BoxFit.fill,
+                image: pokeballImage,
               ),
             ),
-            Center(
-              child: BlocConsumer<LoginCubit, LoginState>(
-                listener: (context, state) {
-                  if (state is LoginErrorState) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        behavior: SnackBarBehavior.floating,
-                        margin: EdgeInsets.all(mediaQuery.size.width * .1),
-                        key: Key('SnackBarDefault'),
-                        backgroundColor: Colors.lightBlue,
-                        content: Text(
-                          state.message,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
+          ),
+          Center(
+            child: BlocConsumer<LoginCubit, LoginState>(
+              listener: (context, state) {
+                if (state is LoginErrorState) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      behavior: SnackBarBehavior.floating,
+                      margin: EdgeInsets.all(mediaQuery.size.width * .1),
+                      key: Key('SnackBarDefault'),
+                      backgroundColor: Colors.lightBlue,
+                      content: Text(
+                        state.message,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
                         ),
-                        duration: Duration(seconds: 2),
                       ),
-                    );
-                  } else if (state is LoginSuscessfullState) {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
-                  }
-                },
-                builder: (context, state) {
-                  if (state is LoginWaitingState) {
-                    return Container(color: Colors.white30, child: CircularProgressIndicator(color: Colors.black));
-                  } else {
-                    return LoginContainer(
-                      loginController: context.read<LoginCubit>().loginController,
-                      passwordController: context.read<LoginCubit>().passwordController,
-                      onpressed: context.read<LoginCubit>().login,
-                    );
-                  }
-                },
-              ),
+                      duration: Duration(seconds: 2),
+                    ),
+                  );
+                } else if (state is LoginSuscessfullState) {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
+                }
+              },
+              builder: (context, state) {
+                if (state is LoginWaitingState) {
+                  return Container(color: Colors.white30, child: CircularProgressIndicator(color: Colors.black));
+                } else {
+                  return LoginContainer(
+                    loginController: context.read<LoginCubit>().loginController,
+                    passwordController: context.read<LoginCubit>().passwordController,
+                    onpressed: context.read<LoginCubit>().login,
+                  );
+                }
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
