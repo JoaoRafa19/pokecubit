e67d5de275d3e155945080f7c8a9ea2456cb9e67
João Rafa <joaopedrorafael19@gmail.com>
2021-08-16 11:39:13 -0300

    logout progress indicator

--- lib/widgets/common/enddrawer.widget.dart
+++ lib/widgets/common/enddrawer.widget.dart
@@ -1,12 +1,31 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:poke_cubit/modules/login/cubit/login_cubit.dart';
 import 'package:poke_cubit/modules/login/login.page.dart';
+import 'package:poke_cubit/widgets/common/sleep.func.dart';
 
 class EndDrawer extends StatelessWidget {
-  const EndDrawer({Key? key}) : super(key: key);
+  EndDrawer({Key? key}) : super(key: key);
+
+  StreamController _streamController = StreamController<String>();
+
+  Stream get output => _streamController.stream;
+  Sink get input => _streamController.sink;
+  bool _loading = false;
+
+  loading() {
+    _loading = true;
+    input.add('loading');
+  }
+
+  concluded() {
+    _loading = false;
+    input.add('concluded');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +36,10 @@ class EndDrawer extends StatelessWidget {
         child: Column(children: <Widget>[
           GestureDetector(
             onTap: () async {
+              loading();
               await FirebaseAuth.instance.signOut();
+              await sleep(2);
+              concluded();
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => BlocProvider(create: (context) => LoginCubit(), child: LoginPage())),
@@ -25,10 +47,17 @@ class EndDrawer extends StatelessWidget {
             },
             child: Row(
               children: [
-                Icon(
-                  Icons.logout,
-                  size: 30,
-                ),
+                StreamBuilder(
+                    stream: this.output,
+                    builder: (context, snapshot) {
+                      if (_loading) {
+                        return CircularProgressIndicator(color: Colors.black);
+                      }
+                      return Icon(
+                        Icons.logout,
+                        size: 30,
+                      );
+                    }),
                 Padding(
                   padding: EdgeInsets.only(left: 10),
                   child: Text(
